b19556cf753eb843a06c4f1c1c291a00b1fb56b0
Charles Mariot <contact@charles-mariot.fr>
2023-05-16 09:26:18 +0200

    Module 01 Python : Bank.py en cours, verifs securite

--- module01/ex05/bank.py
+++ module01/ex05/bank.py
@@ -21,115 +21,126 @@ class Account(object):
 # Fin de la classe account
 
 
-class Bank:
+# Classe Bank : A implementer
+class Bank(object):
+    """The bank"""
 
     def __init__(self):
-        # Accounts Storage
-        self.accounts = {}
+        self.accounts = []
 
-    # Transfer method
-    def transfer(self, origin: Account, destination: Account) -> bool:
-
-        if (self.is_secured(origin) is False
-            or self.is_secured(destination) is False):
+    def add(self, new_account: Account) -> bool:
+        """
+        Add new_account in the Bank
+        @new_account:  Account() new account to append
+        @return   True if success, False if an error occured
+        """
+        # test if new_account is an Account() instance and if
+        # it can be appended to the attribute accounts
+        # ... Your code  ...
+        if not isinstance(new_account, Account):
+            print("Error: new_account is not an Account() instance")
             return False
-
+        for account in self.accounts:
+            if new_account.name == account.name:
+                print("Error: {} name is already used".format(account.name))
+                return False
+        self.accounts.append(new_account)
         return True
 
-    # Security checks method
-    def is_secured(self) -> bool:
+    def __get_accounts_by_name(self,
+                               origin: str,
+                               dest: str) -> 'tuple[Account, Account]':
+        origin_account: Account = None
+        dest_account: Account = None
+        for account in self.accounts:
+            if account.name == origin:
+                origin_account = account
+            elif account.name == dest:
+                dest_account = account
+        if (origin_account is None or dest_account is None):
+            raise FileNotFoundError("Account not found.")
+        return (origin_account, dest_account)
+
+    def __error(error: str) -> bool:
+        print(error)
         return False
 
+    def transfer(self, origin: str, dest: str, amount: float) -> bool:
+        """"
+        Perform the fund transfer
+            @origin:  str(name) of the first account
+            @dest:    str(name) of the destination account
+            @amount:  float(amount) amount to transfer
+            @return   True if success, False if an error occured
+        """
+        if (not isinstance(origin, str)
+            or not isinstance(dest, str)
+                or not isinstance(amount, (float, int))):
+            print("Error: invalid arg type during the transfer.")
+            return False
 
-    def transfer(self, amount):
-        self.value += amount
+        try:
+            (origin_account,
+                dest_account) = self.__get_accounts_by_name(origin,
+                                                            dest)
+        except FileNotFoundError as error:
+            return self.__error(error)
 
-#     def is_secured(self) -> bool:
-        # # Check if has the right objects,
-        # # if it's not corrupted
-        # # and stores enough money to complete the transaction
-        # return True
-        # return False
+        else:
+            if self.__is_account_corrupted(origin_account):
+                # fix origin account
+                return self.__error("Error: origin account corrupted")
+            elif self.__is_account_corrupted(dest_account):
+                # fix dest account
+                return self.__error("Error: dest account corrupted")
+            else:
+                if (origin_account.value < 0 or origin_account.value < amount):
+                    return self.__error("Error: invalid transaction")
 
-    def is_corrupted(self) -> bool:
+    def fix_account(self, name):
+        """
+        fix account associated to name if corrupted
+            @name:   str(name) of the account
+            @return  True if success, False if an error occured
+        """
+        # ... Your code  ...
+
+    def __is_account_corrupted(self, account: Account) -> bool:
         # An account is corrupted if there is :
         # - an even number of attributes
-        if (len(self.__dict__) % 2) == 0:
+        if (len(account.__dict__) % 2) == 0:
             return True
         # - an attribute starting with b
-        elif any(key.startswith('b') for key in self.__dict__):
+        elif any(key.startswith('b') for key in account.__dict__):
             return True
         # - no attribute starting with zip or addr
         elif not any(key.startswith('zip') or key.startswith('addr')
-                     for key in self.__dict__):
+                     for key in account.__dict__):
             return True
         # - no attribute name
-        elif 'name' not in self.__dict__:
+        elif 'name' not in account.__dict__:
             return True
         # - no attribute id
-        elif 'id' not in self.__dict__:
+        elif 'id' not in account.__dict__:
             return True
         # - no attribute value
-        elif 'value' not in self.__dict__:
+        elif 'value' not in account.__dict__:
             return True
         # - name not being a string
-        elif not isinstance(self.name, str):
+        elif not isinstance(account.name, str):
             return True
         # - id not being an int
-        elif not isinstance(self.id, int):
+        elif not isinstance(account.id, int):
             return True
         # - value not being an int or a float
-        elif not isinstance(self.value, (int, float)):
+        elif not isinstance(account.value, (int, float)):
             return True
         return False
-
-
-class Bank:
-    def __init__(self):
-        self.accounts = {}
-
-    def add(self, account: Account) -> bool:
-        if not isinstance(account, Account):
-            return False
-        if account.id in self.accounts:
-            return False
-        self.accounts[account.id] = account
-        return True
-
-    def transfer(self, origin: Account, dest: Account, amount: float) -> bool:
-        if not isinstance(origin, Account) or not isinstance(dest, Account):
-            return False
-        if origin.id not in self.accounts or dest.id not in self.accounts:
-            return False
-        if origin.value < amount:
-            return False
-        origin.value -= amount
-        dest.transfer(amount)
-        return True
 
 
 if __name__ == "__main__":
     bank = Bank()
+    bank.add(Account("Smith", value=10000, zip="94700"))
+    bank.add(Account("Doe", value=1000, zip="91000"))
 
-    bank.add(Account(
-        'Smith Jane',
-        zip='911-745',
-        value=1000.0,
-        bref='1044618427ff2782f0bbece0abd05f31'
-    ))
-
-    bank.add(Account(
-        'William John',
-        zip='100-064',
-        value=6460.0,
-        ref='58ba2b9954cd278eda8a84147ca73c87',
-        info=None,
-        other='This is the vice president of the corporation'
-    ))
-
-    print("Accounts: ", bank.accounts[1].name)
-
-    # if bank.transfer('William John', 'Smith Jane', 545.0) is False:
-    #     print('Failed')
-    # else:
-    #     print('Success')
+    bank.transfer("Smith", "truc", 500)
