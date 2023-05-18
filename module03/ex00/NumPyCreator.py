6713e3e3e3c2ca30b6deb0a571dc754adca390c7
Charles Mariot <contact@charles-mariot.fr>
2023-05-18 19:28:03 +0200

    Python Module 03 : Tests sur ex00, les options sont a verifier pour la creation de tableaux Numpy

--- module03/ex00/NumPyCreator.py
+++ module03/ex00/NumPyCreator.py
@@ -2,12 +2,18 @@ import numpy as np
 
 
 class NumPyCreator:
+    """
+    NumPyCreator class
+    Creates NumPy arrays from various inputs.
+    """
 
     def from_list(self,
-                  lst: 'list',
-                  dtype=list) -> 'np.array':
+                  lst: 'list' = [],
+                  dtype=object) -> 'np.array':
         """
         takes in a list and returns its corresponding NumPy array.
+        a list of numbers will create a 1D array,
+        a list of list will create a 2D array,
         Args:
             lst: list
         Returns:
@@ -16,8 +22,8 @@ class NumPyCreator:
         return np.array(lst, dtype=dtype)
 
     def from_tuple(self,
-                   tpl: 'tuple',
-                   dtype=tuple) -> 'np.array':
+                   tpl: 'tuple' = (),
+                   dtype=None) -> 'np.array':
         """
         takes in a tuple and returns its corresponding NumPy array.
         Args:
@@ -28,8 +34,8 @@ class NumPyCreator:
         return np.array(tpl, dtype=dtype)
 
     def from_iterable(self,
-                      itr: 'np.iterable',
-                      dtype=object) -> 'np.array':
+                      itr: 'np.iterable' = [],
+                      dtype=None) -> 'np.array':
         """
         takes in an iterable and returns an array which contains all
         of its elements.
@@ -43,7 +49,7 @@ class NumPyCreator:
     def from_shape(self,
                    shape: 'tuple[int, int]',
                    value=0,
-                   dtype=object) -> 'np.array':
+                   dtype=None) -> 'np.array':
         """
         returns an array filled with the same value.
         Args:
@@ -67,7 +73,7 @@ class NumPyCreator:
 
     def identity(self,
                  n: int,
-                 dtype=object) -> 'np.array':
+                 dtype=float) -> 'np.array':
         """
         returns an array representing the identity matrix of size n.
         Args:
@@ -84,14 +90,53 @@ if __name__ == "__main__":
 
     shape = (3, 5)
 
-    print(npc.from_list([[1, 2, 3], [6, 3, 4]]))
-    print(npc.from_list([[1, 2, 3], [6, 4]]))
+    print("FROM LIST")
+    print("\n0 x 0 array :\n",
+          npc.from_list())
+    print("\n1 x 0 array :\n",
+          npc.from_list([]))
+    print("\n1 x 1 array :\n",
+          npc.from_list([[42]]))
+    print("\n1 x 6 array :\n",
+          npc.from_list([[1, 2, 3, 6, 3, 4]]))
+    print("\n2 x 3 array :\n",
+          npc.from_list([[1, 2, 3], [6, 3, 4]]))
+    print("\n3 x 3 array :\n",
+          npc.from_list([[1, 2, 3], [6, 3, 4], [1, 2, 3]]))
+    print("\nLines of different lengths:\n",
+          npc.from_list([[1, 2, 3], [6, 4]]))
 
-    print(npc.from_tuple(("a", "b", "c")))
-    print(npc.from_tuple(["a", "b", "c"]))
+    print("\nFROM TUPLE")
+    print("\n0 x 0 array :\n",
+          npc.from_tuple())
+    print("\n1 x 0 array :\n",
+          npc.from_tuple(()))
+    print("\n1 x 1 array :\n",
+          npc.from_tuple((42,)))
+    print("\n1 x 6 array :\n",
+          npc.from_tuple((1, 2, 3, 6, 3, 4)))
+    print("\n2 x 3 array :\n",
+          npc.from_tuple(((1, 2, 3), (6, 3, 4))))
+    print("\n3 x 3 array :\n",
+          npc.from_tuple(((1, 2, 3), (6, 3, 4), (2, 5, 3))))
 
-    print(npc.from_iterable(range(5)))
+    print("\nFROM ITERABLE")
+    print("\n0 x 0 array :\n",
+          npc.from_iterable(range(5, 0)))
+    print("\n1 x 0 array :\n",
+          npc.from_iterable(range(0, 5)))
+    print("\n1 x 3 array :\n",
+          npc.from_iterable(range(0, 6, 2)))
+    print("\nfrom str",
+          npc.from_iterable("test"))
+    print("",
+          npc.from_iterable((1, 2, 3, 6, 3, 4)))
+    print("",
+          npc.from_iterable([[1, 2, 3], [6, 3, 4]]))
+    print("",
+          npc.from_iterable(((1, 2, 3), (6, 3, 4))))
 
+    print("\nFROM SHAPE")
     print(npc.from_shape(shape))
 
     print(npc.random(shape))
