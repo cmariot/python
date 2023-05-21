dda549761f12cb90538e6e94188da90576e031d0
Charles Mariot (42) <contact@charles-mariot.fr>
2023-05-21 13:18:56 +0200

    backup

--- module03/ex01/ImageProcessor.py
+++ module03/ex01/ImageProcessor.py
@@ -1,22 +1,29 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
 
 
 class ImageProcessor:
 
     def load(self, path: str):
+        """
+        Takes as a parameter a path to an image
+        and returns a numpy array representing the image
+        """
         img = mpimg.imread(path)
-        #print("Loading image of dimensions {} x {}".format(img.size[0], img.size[1]))
+        print("Loading image of dimensions {} x {}".format(img.shape[0], img.shape[1]))
         return np.array(img)
 
     def display(self, array):
+        """
+        Takes a numpy array as a parameter and displays
+        the corresponding RGB image
+        """
         plt.imshow(array)
         plt.show()
-        return mpimg.fr
+
 
 if __name__ == "__main__":
     imp = ImageProcessor()
     arr = imp.load("./elon_canaGAN.png")
     imp.display(arr)
-    
